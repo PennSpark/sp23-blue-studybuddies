abd0daa53b04a84369f90217e7fc711230542d88
andrevandeven <andrev@sas.upenn.edu>
2023-03-06 16:03:22 -0500

    rest framework

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import PostSerializer
+from .models import Post
 
 # Create your views here.
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('published_date')
+    serializer_class = PostSerializer
